2dc5c34c7fc6f4a865f5dd4e73917f6165aea48c
ta2edchimp <kontakt@andreaswindt.de>
2016-02-20 12:29:15 +0100

    Added options, lookup of status codes.

--- app/index.js
+++ app/index.js
@@ -17,6 +17,80 @@ colors.setTheme( {
   error: [ 'red', 'bold' ]
 } );
 
+
+if ( process.argv.indexOf( '-v' ) > 0 || process.argv.indexOf( '--version' ) > 0 ) {
+
+  console.info(
+    getVersion()
+  );
+
+} else if ( process.argv.indexOf( '-h' ) > 0 || process.argv.indexOf( '--help' ) > 0 ) {
+
+  console.info(
+    getUsageInfo( process.argv )
+  );
+
+} else if ( process.argv.length > 2 ) {
+
+  console.info(
+    lookUpHttpStatusCode( process.argv.splice( 2 ) )
+  );
+
+} else {
+
+  chooseHttpStatusCode();
+
+}
+
+
+/**
+ * Returns the current version number.
+ *
+ * @return {string}
+ */
+function getVersion() {
+  return require( '../package.json' ).version;
+}
+
+
+/**
+ * Prints out "What The... Status Code" usage info.
+ *
+ * @param  {Array} args Arguments the Script has been called with
+ * @return {string}
+ */
+function getUsageInfo( args ) {
+  var
+    fullPath = ( args[ 1 ] || 'what-the-status-code' ).split( '/' ),
+    name = fullPath[ fullPath.length - 1 ],
+    version = getVersion(),
+
+    padLeft = '  ',
+
+    usageInfo = [];
+
+  usageInfo.push( name + ' v' + version + '\n' );
+
+  usageInfo.push( 'Without arguments:\n' );
+  usageInfo.push( padLeft + 'Invoking ' + name + ' without any arguments will ask' );
+  usageInfo.push( padLeft + 'a few questions prior to suggesting an HTTP Status Code' );
+  usageInfo.push( padLeft + 'to meet your situation.\n\n' );
+
+  usageInfo.push( 'Looking up Status Codes:\n' );
+  usageInfo.push( padLeft + 'Invoke ' + name + ' with any number of HTTP Status Codes' );
+  usageInfo.push( padLeft + 'to look up their message and meaning.\n' );
+  usageInfo.push( padLeft + 'Example:' );
+  usageInfo.push( padLeft + name + ' 200 404 504\n\n' );
+
+  usageInfo.push( 'Miscellaneous:\n' )
+  usageInfo.push( padLeft + '-h, --help     Show help' );
+  usageInfo.push( padLeft + '-v, --version  Outputs the version number' );
+  usageInfo.push( padLeft + '--no-color     Deactivates colorized output\n' );
+
+  return usageInfo.join( '\n' );
+}
+
+
 /**
  * Receives a single numeric HTTP Status Code or an Array of those, determines
  * its/their message and meaning and transforms these results into a
@@ -37,6 +111,7 @@ function getStatusCodesTable( codes ) {
     len = _codes.length,
     code = 0,
     codeInfo = {},
+    message = '',
     meaning = '',
     resultsTable = new Table( {
       head: [
@@ -57,18 +132,26 @@ function getStatusCodesTable( codes ) {
 
     codeInfo = statusCodes[ code ];
 
-    if ( !codeInfo ) {
-      throw Error( 'HTTP Status Code "' + code + '" could not be resolved!' );
-    }
+    if ( codeInfo ) {
 
-    meaning = codeInfo.meaning;
-    if ( codeInfo.link === true ) {
-      meaning += '\n' + ( 'https://httpstatuses.com/' + code ).link;
+      message = codeInfo.message;
+      meaning = codeInfo.meaning;
+
+      if ( codeInfo.link === true ) {
+        meaning += '\n' + ( 'https://httpstatuses.com/' + code ).link;
+      }
+
+    } else {
+
+      code = ( '' + code ).error;
+      message = '-'.error;
+      meaning = 'This code is unknown or not an HTTP Status Code.'.error;
+
     }
 
     resultsTable.push( [
       code,
-      codeInfo.message,
+      message,
       meaning
     ] );
   }
@@ -76,26 +159,52 @@ function getStatusCodesTable( codes ) {
   return resultsTable;
 }
 
-traverse( questions )
-  .then( function onResolved( value ) {
-    var
-      outputTable;
 
-    if ( !value ) {
-      throw Error( 'Could not resolve HTTP Status Code!' );
-    }
+/**
+ * Looks up a given list of numeric values for their corresponding meaning as
+ * HTTP Status Codes.
+ *
+ * @param  {Array} lookup Array of numbers to look up
+ * @return {string}
+ */
+function lookUpHttpStatusCode( lookup ) {
+  var
+    codes = lookup.filter( function filterNaNs( n ) {
+      return !isNaN( n );
+    } );
 
-    outputTable = getStatusCodesTable( value );
+  return getStatusCodesTable( codes ).toString();
+}
 
-    if ( !( outputTable instanceof Table ) ) {
-      throw Error( 'Could not transform HTTP Status Code(s) ' + value + ' into a table to output!' );
-    }
 
-    console.log( getStatusCodesTable( value ).toString() );
-  } )
-  .catch( function onRejected( reason ) {
-    console.error(
-      'What The...\n'.error,
-      reason
-    );
-  } );
+/**
+ * Handles the process of asking the defined questions to determine an adequate
+ * HTTP Status Code.
+ *
+ * @return {void}
+ */
+function chooseHttpStatusCode() {
+  traverse( questions )
+    .then( function onResolved( value ) {
+      var
+        outputTable;
+
+      if ( !value ) {
+        throw Error( 'Could not resolve HTTP Status Code!' );
+      }
+
+      outputTable = getStatusCodesTable( value );
+
+      if ( !( outputTable instanceof Table ) ) {
+        throw Error( 'Could not transform HTTP Status Code(s) ' + value + ' into a table to output!' );
+      }
+
+      console.info( outputTable.toString() );
+    } )
+    .catch( function onRejected( reason ) {
+      console.error(
+        'What The...\n'.error,
+        reason
+      );
+    } );
+}
