9065bcca097f0ddef9d148679d620fb1682d4d37
ta2edchimp <kontakt@andreaswindt.de>
2016-02-20 12:28:46 +0100

    ...

--- app/status-codes.js
+++ app/status-codes.js
@@ -199,7 +199,7 @@ module.exports = {
     link: true
   },
   '418': {
-    message: 'I\'m a teapot',
+    message: 'I\'m a Teapot',
     meaning: 'Any attempt to brew coffee with a teapot should result in the error code "418 I\'m a teapot". The resulting entity body MAY be short and stout.',
     link: true
   },
